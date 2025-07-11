1dfbd9207be1e67ab86f05f1bb83cac5c20b6583
githubVivek55 <vivekpce55@outlook.com>
2025-07-11 22:05:28 +0530

    day 44 amd 45 import module nad main

---
+++ common.py
@@ -0,0 +1,11 @@
+
+def Welcome():
+    print("Welcome to the Python training program!")
+def Farewell():
+    print("Thank you for participating in the Python training program! Goodbye!")
+
+print(__name__)
+
+if __name__ == "__main__":
+    Welcome()
+    Farewell()
---
+++ day_44_45.py
@@ -0,0 +1,12 @@
+# import package in python
+import pandas as pd
+from math import sqrt as math_sqrt
+import common as common
+
+# Use the package
+math_result = math_sqrt(16)
+print(math_result)
+
+
+common.Welcome()
+common.Farewell()
