7b81a71e55558fb9c65b6ab8fb8c51535c00d8d2
githubVivek55 <vivekpce55@outlook.com>
2025-08-28 22:41:10 +0530

    day:50 file handling

--- day_49.py
+++ day_49.py
@@ -8,9 +8,23 @@ def read_file(file_path):
         content = file.read()
     return content
 
+def file_handling():
+    file_path = "data/sampleFile.csv"
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            print(lines)
+            for line in lines:
+                cols = line.strip().split(',')
+                s = [int(c) for c in cols if c.isdigit()]
+                print(s)
+
+
+
+
 if __name__ == "__main__":
     file_path = "data/sampleFile.csv"
     file_content = read_file(file_path)
-    print(f"File content:\n{file_content}")
+    file_handling()
 
 
